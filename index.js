cd453401e69da80fd9f4b7476304ecad082d4872
Gabriela Reyes <gabrielareyesg92@gmail.com>
2023-03-03 21:25:59 -0500

    Actualizacion proyecto

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+
 function convertir (){
-    const valore = parseInt(document.getElementById("valor").innerHTML);
-    let resultado = 0;
-    const dolar = 4.911;
-    const euro = 5.246;
-    if (document.getElementById("one").checked){
-        resultado = valore / dolar;
-        alert ("The change from Colombian pesos to dollars is: $" + resultado);
-    }
-    else if (document.getElementById("two").checked){
-        resultado = valore / euro;
-        alert ("The change from Colombian pesos to euro is: €" + resultado);
+let resultado 
+const dolar = 4911;
+const euro = 5246;
+const operacion = prompt ("What type of change do you want?")
+ if(operacion ==="dolars"){
+  const dolart = parseInt(prompt ("Enter the amount"))
+ resultado = dolart / dolar
+ alert ("The change from Colombian pesos to dollars is: $" + resultado)
+ }
+else if (operacion === "euros"){
+    const eurot = parseInt(prompt ("Enter the amount"))
+    resultado = eurot / euro
+    alert ("The change from Colombian pesos to euro is: €" + resultado)
     }
     else {
-        alert ("You need to complete all the requirements")
-    }
+       alert ("You need to complete all the requirements")
 }
+
+}
+
+console.log (resultado)
