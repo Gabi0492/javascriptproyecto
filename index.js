5509a4d7b6027f3cf162b495a19e3bd961b622f7
Gabriela Reyes <gabrielareyesg92@gmail.com>
2023-03-13 19:33:54 -0500

    Actualizacion segunda preentrega

--- index.js
+++ index.js
@@ -1,23 +1,70 @@
+//Array//
 
-function convertir (){
-let resultado 
-const dolar = 4911;
-const euro = 5246;
-const operacion = prompt ("What type of change do you want?")
- if(operacion ==="dolars"){
-  const dolart = parseInt(prompt ("Enter the amount"))
- resultado = dolart / dolar
- alert ("The change from Colombian pesos to dollars is: $" + resultado)
- }
-else if (operacion === "euros"){
-    const eurot = parseInt(prompt ("Enter the amount"))
-    resultado = eurot / euro
-    alert ("The change from Colombian pesos to euro is: €" + resultado)
+const productos = [
+    {nombre:"Shoes", precio: 1000},
+    {nombre:"Tshirt", precio: 1000},
+    {nombre:"Pants", precio: 1000},
+
+
+
+];
+//Inicio de las variables //
+
+let carrito = []
+let seleccion = prompt ("Hi do you want to buys something? Please select yes or no")
+
+while(seleccion != "yes" && seleccion != "no"){
+    alert ("Please select yes or no")
+    seleccion = prompt ("Do you want to buy something yes or no")
+}
+
+if (seleccion == "yes"){
+    alert ("Attache the list of the products")
+    let todosLosProductos = productos.map(
+        (producto) => producto.nombre + "" + producto.precio + "$"
+    );
+    alert (todosLosProductos.join(" - "))
+}else if (seleccion == "no"){
+    alert ("Thanks for coming")
+}
+
+// Inicio del segundo bucle//
+
+while (seleccion == "yes"){
+let producto = prompt ("Do you want to add something to the cart")
+let precio = 0
+
+if (producto == "Shoes" || producto == "Tshirt" || producto == "Pants"){
+    switch (producto){
+        case "Shoes":
+        precio = 1000;
+        break;
+        case "Tshirt":
+        precio = 1000;
+        break;
+        case "Pants":
+        precio = 1000;
+        break;
+    
     }
-    else {
-       alert ("You need to complete all the requirements")
-}
+    let unidades = parseInt (prompt("How many pieces do you want to add"))
+    carrito.push ({producto, unidades, precio})
+    console.log (carrito)
+    }else{
+        alert ("We dont have that product")
 
 }
+seleccion = prompt ("Do you want to continue buying")
 
-console.log (resultado)
+//Ultimo bucle//
+while (seleccion === "no"){
+    alert ("Thanks for your time")
+    carrito.forEach((carritoFinal) =>{
+        console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, final amount ${carritoFinal.unidades * carritoFinal.precio}`)
+    })
+    break;
+}
+}
+
+const total = carrito.reduce ((acc, el) => acc + el.precio * el.unidades, 0)
+console.log (`Total of your buys is: ${total}`)
